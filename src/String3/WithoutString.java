2981d5bbce39dc791ddf0c6bda07f1c5a5034bc8
Sudharsan Rajendran <sudharsxn@gmail.com>
2021-10-11 21:42:07 +1100

    without string - two tests failing tho

---
+++ src/String3/WithoutString.java
@@ -0,0 +1,50 @@
+package String3;
+/*
+Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive).
+You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+
+
+withoutString("Hello there", "llo") → "He there"
+withoutString("Hello there", "e") → "Hllo thr"
+withoutString("Hello there", "x") → "Hello there"
+
+ */
+public class WithoutString {
+    public static void main(String[] args) {
+        WithoutString test = new WithoutString();
+        System.out.println(test.withoutString("This is a FISH", "IS"));
+    }
+
+    public String withoutString(String base, String remove) {
+        StringBuilder sb = new StringBuilder(base);
+        System.out.println("before: " + sb);
+
+        sb = removeInd(sb, remove.toLowerCase());
+        sb = removeInd(sb, remove.toUpperCase());
+        /*
+        int index = sb.indexOf(remove.toLowerCase());
+        while (index != -1) {
+            sb.delete(index, index + (remove.length()));
+            index = sb.indexOf(remove.toLowerCase());
+        }
+
+        int index1 = sb.indexOf(remove.toUpperCase());
+        while (index1 != -1) {
+            sb.delete(index1, index1 + (remove.length()));
+            index1 = sb.indexOf(remove.toUpperCase());
+        }
+        */
+
+        System.out.println("after: " + sb);
+        return sb.toString();
+    }
+
+    public StringBuilder removeInd(StringBuilder sb, String remove) {
+        int index = sb.indexOf(remove);
+        while (index != -1) {
+            sb.delete(index, index + (remove.length()));
+            index = sb.indexOf(remove);
+        }
+        return sb;
+    }
+}
