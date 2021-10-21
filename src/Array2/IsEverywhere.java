7c339d717b752c35f3629a17040db4bed13d424e
Sudharsan Rajendran <sudharsxn@gmail.com>
2021-10-21 22:36:39 +1100

    is Everywhere

--- src/Array2/IsEverywhere.java
+++ src/Array2/IsEverywhere.java
@@ -14,35 +14,33 @@ import java.util.ArrayList;
 public class IsEverywhere {
     public static void main(String[] args) {
         IsEverywhere test = new IsEverywhere();
-        int[] x = {2, 1, 2, 2, 2, 1, 2, 1};
-        System.out.println(test.isEverywhere(x, 2));
+        int[] x = {2, 1, 2, 1};
+        System.out.println(test.isEverywhere(x, 1));
     }
 
     public boolean isEverywhere(int[] nums, int val) {
         ArrayList<Integer> arr = new ArrayList<Integer>();
         for (int each: nums) { arr.add(each); }
-        System.out.println("My arr: " + arr);
 
         int x = arr.indexOf(val);
-        System.out.println("My X: " + x);
-
-        int check = -1;
-        if (x % 2 == 0 ) { check = 2; } // If it is even index
-        else { check = 3; } // If it is odd index
-
         int valCount = 0;
         int valExistCount = 0;
+
         for (int i=0; i<arr.size(); i++) {
-            if (i%check==0) {  //check corresponding index
+            if (x%2==0 && i%2==0) {
+                valCount += 1;
+                if (arr.get(i) == val) {
+                    valExistCount += 1;
+                }
+            }
+
+            if (x%2!=0 && i%2!=0) {
                 valCount += 1;
                 if (arr.get(i) == val) {
                     valExistCount += 1;
                 }
             }
         }
-
-        System.out.print("ValCount: " + valCount);
-        System.out.println(" <--> ValExisit: " + valExistCount);
 
         return (valCount == valExistCount);
     }
