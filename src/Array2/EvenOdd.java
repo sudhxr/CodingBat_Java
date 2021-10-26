f4f132bae6b4baebb8e2ee32b41db50fdcb086a3
Sudharsan Rajendran <sudharsxn@gmail.com>
2021-10-26 14:01:57 +1100

    EvenOdd v1

---
+++ src/Array2/EvenOdd.java
@@ -0,0 +1,33 @@
+package Array2;
+/*
+Return an array that contains the exact same numbers as the given array, but rearranged so that all the even numbers
+come before all the odd numbers. Other than that, the numbers can be in any order.
+You may modify and return the given array, or make a new array.
+
+evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
+evenOdd([3, 3, 2]) → [2, 3, 3]
+evenOdd([2, 2, 2]) → [2, 2, 2]
+ */
+
+public class EvenOdd {
+    public int[] evenOdd(int[] nums) {
+        int[] arr = new int[nums.length];
+        int index = 0;
+
+        for (int each: nums) {
+            if (each % 2 == 0) {
+                arr[index] = each;
+                index += 1;
+            }
+        }
+
+        for (int each: nums) {
+            if (each%2 != 0) {
+                arr[index] = each;
+                index+=1;
+            }
+        }
+
+        return arr;
+    }
+}
