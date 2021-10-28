2fa3b6e1437731e19fbaecca7b88d79b6af2eca3
Sudharsan Rajendran <sudharsxn@gmail.com>
2021-10-28 19:41:45 +1100

    max mirror

--- src/Array3/MaxMirror.java
+++ src/Array3/MaxMirror.java
@@ -7,27 +7,28 @@ import java.util.Collections;
 public class MaxMirror {
     public static void main(String[] args) {
         MaxMirror test = new MaxMirror();
-        int[] x = {1, 2, 1};
+        int[] x = {1, 2, 1, 4};
         System.out.println(test.maxMirror(x));
     }
 
     public int maxMirror(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        ArrayList<Integer> revArr = new ArrayList<Integer>();
+        int max = 0;
 
-        for (int each: nums) {
-            arr.add(each);
-            revArr.add(each);
+        for (int i=0; i<nums.length; i++) {
+            int count = 0;
+            for (int j=nums.length-1; j>=0 && i + count < nums.length; j--) {
+                if (nums[j] == nums[i + count]) {
+                    count += 1;
+                }
+                else {
+                    max = Math.max(max, count);
+                    count = 0;
+                }
+            }
+            max = Math.max(max, count);
         }
-        Collections.reverse(revArr);
 
-        if (arr.equals(revArr)) {
-            return arr.size();
-        }
-        System.out.println("Test" + arr);
-        System.out.println("Test" + revArr);
-
-        return -1;
+        return max;
     }
 
 }
